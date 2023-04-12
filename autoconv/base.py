a5428b7d9ee6423b33a1c22363b6eaa6bb5e492b
Blake Dewey <blake.dewey@jhu.edu>
2023-04-11 21:28:32 -0400

    bad regex from refactor

--- autoconv/base.py
+++ autoconv/base.py
@@ -246,7 +246,7 @@ class BaseInfo:
             (r'(cerv|c[ -]?sp|c.?spine|msma)', 'CSPINE', r'(thor|t[ -]?sp|t.?spine)', 'SPINE'),
             (r'(thor|t[ -]?sp|t.?spine)', 'TSPINE', r'(cerv|c[ -]?sp|c.?spine)', 'SPINE'),
             (r'(lumb|l[ -]?sp|l.?spine)', 'LSPINE'),
-            (r'(\sc.?tl?(?:\s+|$)|^sp_|\(t1.ax.vibe|t1.vibe.tra|ax.t1.vibe\))', 'SPINE'),
+            (r'(\sc.?tl?(?:\s+|$)|^sp_|t1.ax.vibe|t1.vibe.tra|ax.t1.vibe)', 'SPINE'),
             (r'(orbit|thin|^on_)', 'ORBITS'),
             (r'spine', 'SPINE'),
         ]
