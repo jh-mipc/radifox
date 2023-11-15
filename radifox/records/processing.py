aab27d8e808a68afe4174272d57829da33bec855
Blake Dewey <blake.dewey@jhu.edu>
2023-11-15 10:12:53 -0500

    only make QA images from NIfTI files

--- radifox/records/processing.py
+++ radifox/records/processing.py
@@ -155,6 +155,8 @@ class ProcessingModule(ABC):
         out_dir = outs[0].parent.parent / "qa" / name
         out_dir.mkdir(exist_ok=True, parents=True)
         for out in outs:
+            if not str(out).endswith(".nii.gz"):
+                continue
             create_qa_image(str(out), out_dir / f"{out.name.split('.')[0]}.png")
 
     def generate_qa_images(self) -> None:
