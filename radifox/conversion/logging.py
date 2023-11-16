4cb1ebf011502de51d6d91bd7fba53232b5c3fc5
Blake Dewey <blake.dewey@jhu.edu>
2023-11-15 20:33:31 -0500

    add timestamp to log filename

--- radifox/conversion/logging.py
+++ radifox/conversion/logging.py
@@ -1,3 +1,4 @@
+import datetime
 import logging
 from pathlib import Path
 import sys
@@ -24,8 +25,9 @@ def create_loggers(
 ) -> None:
     mkdir_p(log_dir)
     log_formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
+    timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
     # noinspection PyTypeChecker
-    log_file = logging.FileHandler(log_dir / f"{log_prefix}-info.log", delay=True)
+    log_file = logging.FileHandler(log_dir / f"{log_prefix}-{timestamp}-info.log", delay=True)
     log_file.setFormatter(log_formatter)
     logging.getLogger().setLevel(logging.DEBUG if verbose else logging.INFO)
     logging.getLogger().addHandler(log_file)
@@ -34,14 +36,14 @@ def create_loggers(
         log_stream.setFormatter(log_formatter)
         logging.getLogger().addHandler(log_stream)
     # noinspection PyTypeChecker
-    warn_file = logging.FileHandler(log_dir / f"{log_prefix}-warnings.log", delay=True)
+    warn_file = logging.FileHandler(log_dir / f"{log_prefix}-{timestamp}-warning.log", delay=True)
     warn_file.addFilter(LogFilter(logging.ERROR - 1))
     logging.addLevelName(WARNING_DEBUG, "WARNING-DEBUG")
     warn_file.setFormatter(log_formatter)
     warn_file.setLevel(WARNING_DEBUG if verbose else logging.WARNING)
     logging.getLogger().addHandler(warn_file)
     # noinspection PyTypeChecker
-    error_file = logging.FileHandler(log_dir / f"{log_prefix}-errors.log", delay=True)
+    error_file = logging.FileHandler(log_dir / f"{log_prefix}-{timestamp}-error.log", delay=True)
     error_file.addFilter(LogFilter(logging.ERROR))
     error_file.setFormatter(log_formatter)
     error_file.setLevel(logging.ERROR)
