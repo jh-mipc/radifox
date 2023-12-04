9865ae407d018d7dd9934a1c1eba1fed07f4322f
Blake Dewey <blake.dewey@jhu.edu>
2023-12-04 10:41:22 -0500

    add skip-set-sform option

--- radifox/modules/staging.py
+++ radifox/modules/staging.py
@@ -14,7 +14,6 @@ from .._version import __version__
 from ..naming import ImageFile, ImageFilter, glob
 from ..records import ProcessingModule
 
-
 __all__ = ["Staging", "StagingPlugin"]
 
 
@@ -32,6 +31,7 @@ class Staging(ProcessingModule):
         parser.add_argument("--keep-best-res", action="store_true", default=False)
         parser.add_argument("--plugin-paths", type=Path, nargs="+", default=None)
         parser.add_argument("--skip-default-plugins", action="store_true", default=False)
+        parser.add_argument("--skip-set-sform-qform", action="store_true", default=False)
         parsed = parser.parse_args(args)
 
         parsed.subject_dir = parsed.subject_dir.resolve()
@@ -81,21 +81,35 @@ class Staging(ProcessingModule):
             "reg_filters": [parsed.reg_filters] * len(session_imgs),
             "plugin_paths": [parsed.plugin_paths] * len(session_imgs),
             "skip_default_plugins": [parsed.skip_default_plugins] * len(session_imgs),
+            "skip_set_sform": [parsed.skip_set_sform] * len(session_imgs),
         }
 
     @staticmethod
     def run(
-            session_filepaths: list[list[ImageFile]],
-            image_types: list[list[ImageFilter]],
-            keep_best_res: list[bool],
-            plugin_paths: list[list[Path]],
-            reg_filters: list[list[ImageFilter] | None],
-            skip_default_plugins: list[bool],
+        session_filepaths: list[list[ImageFile]],
+        image_types: list[list[ImageFilter]],
+        keep_best_res: list[bool],
+        plugin_paths: list[list[Path]],
+        reg_filters: list[list[ImageFilter] | None],
+        skip_default_plugins: list[bool],
+        skip_set_sform: list[bool],
     ):
         # For each session, find images that match the contrast filters
         session_imgs = {}
-        for all_imgs, img_filters, best_res, proc_plugin_paths, skip_defaults in zip(
-                session_filepaths, image_types, keep_best_res, plugin_paths, skip_default_plugins
+        for (
+            all_imgs,
+            img_filters,
+            best_res,
+            proc_plugin_paths,
+            skip_defaults,
+            skip_set_sform_qform,
+        ) in zip(
+            session_filepaths,
+            image_types,
+            keep_best_res,
+            plugin_paths,
+            skip_default_plugins,
+            skip_set_sform,
         ):
             # Create "stage" directory
             session = all_imgs[0].parent.parent
@@ -143,15 +157,16 @@ class Staging(ProcessingModule):
                             # img.path.unlink()
                 # Add image(s) to filtered image list
                 filtered_imgs.extend(imgs)
-            # Fix sform/qform of filtered images
-            out_imgs = [fix_sform_qform(img) for img in filtered_imgs]
+            if not skip_set_sform_qform:
+                # Fix sform/qform of filtered images
+                filtered_imgs = [fix_sform_qform(img) for img in filtered_imgs]
 
             # Remove staged images that are not in the filtered image list
             for stage_img in (session / "stage").iterdir():
-                if stage_img.name not in [img.name for img in out_imgs]:
+                if stage_img.name not in [img.name for img in filtered_imgs]:
                     stage_img.unlink()
 
-            session_imgs[session] = out_imgs
+            session_imgs[session] = filtered_imgs
 
         reg_filters = reg_filters[0]
         if reg_filters is None:
@@ -172,9 +187,7 @@ class Staging(ProcessingModule):
                     # Currenly use private _filter_dict to check acqdim
                     # Radifox should be updated to expose these parameters readonly
                     best_func = (
-                        pick_most_isotropic
-                        if reg_filter.acqdim == "3D"
-                        else pick_smallest_slices
+                        pick_most_isotropic if reg_filter.acqdim == "3D" else pick_smallest_slices
                     )
                     filtered = reg_filter.filter(imgs)
                     if filtered:
@@ -262,8 +275,11 @@ def load_plugins(plugin_path: Path) -> list[StagingPlugin]:
     spec.loader.exec_module(module)
 
     # Inspect the module and find all subclasses of the specified base class
-    return [member for name, member in inspect.getmembers(module, inspect.isclass)
-            if issubclass(member, StagingPlugin) and member is not StagingPlugin]
+    return [
+        member
+        for name, member in inspect.getmembers(module, inspect.isclass)
+        if issubclass(member, StagingPlugin) and member is not StagingPlugin
+    ]
 
 
 class StagingPlugin(ABC):
@@ -347,8 +363,7 @@ class MP2RAGEPlugin(StagingPlugin):
         for img in imgs:
             inv = next(ex for ex in img.extras if "INV" in ex)
             cmplx_type = next(
-                (ct for ct in MP2RAGEPlugin.CMPLX_IMG_TYPES if ct in img.extras),
-                "MAG"
+                (ct for ct in MP2RAGEPlugin.CMPLX_IMG_TYPES if ct in img.extras), "MAG"
             )
             img_dict[f"{inv}-{cmplx_type}"].append(img)
         data = {
@@ -363,9 +378,9 @@ class MP2RAGEPlugin(StagingPlugin):
             temp_img = img_dict["INV1-MAG"][0]
             for inv in ["INV1", "INV2"]:
                 data[f"{inv}-PHA"] = (
-                        (data["INV1-PHA"] - data["INV1-PHA"].min())
-                        / (data["INV1-PHA"].max() - data["INV1-PHA"].min())
-                        * (2 * np.pi)
+                    (data["INV1-PHA"] - data["INV1-PHA"].min())
+                    / (data["INV1-PHA"].max() - data["INV1-PHA"].min())
+                    * (2 * np.pi)
                 )
                 data[f"{inv}-CPX"] = data[f"{inv}-MAG"] * np.exp(data[f"{inv}-PHA"] * 1j)
         else:
