0e4e959cc72826a140ec6136c186d61370e8ae09
Blake Dewey <blake.dewey@jhu.edu>
2026-03-17 12:41:13 -0400

    bug in generators

--- radifox/records/resize.py
+++ radifox/records/resize.py
@@ -14,21 +14,18 @@ def nn_resize_1mmiso(img_obj):
 
     # Compute the new shape based on the scaling factors
     new_shape = tuple(
-        int(round(dim / scale)) for dim,
-        scale in zip(img_obj.shape, scaling_factors),
+        int(round(dim / scale)) for dim, scale in zip(img_obj.shape, scaling_factors)
     )
 
     # Define the original bounds of the image
     original_bounds_start = (-0.5,) * len(data.shape)
     original_bounds_end = tuple(
-        start + dim for start,
-        dim in zip(original_bounds_start, img_obj.shape),
+        start + dim for start, dim in zip(original_bounds_start, img_obj.shape)
     )
 
     # Calculate the original and new sizes
     original_size = [
-        end - start for start,
-        end in zip(original_bounds_start, original_bounds_end),
+        end - start for start, end in zip(original_bounds_start, original_bounds_end)
     ]
     new_size = [dim * scale for dim, scale in zip(new_shape, scaling_factors)]
 
@@ -37,12 +34,10 @@ def nn_resize_1mmiso(img_obj):
 
     # Adjust the bounds to account for the size differences
     adjusted_bounds_start = tuple(
-        start + diff for start, 
-        diff in zip(original_bounds_start, size_diff),
+        start + diff for start, diff in zip(original_bounds_start, size_diff)
     )
     adjusted_bounds_end = tuple(
-        end - diff for end,
-        diff in zip(original_bounds_end, size_diff),
+        end - diff for end, diff in zip(original_bounds_end, size_diff)
     )
 
     # Generate coordinates for the new grid
